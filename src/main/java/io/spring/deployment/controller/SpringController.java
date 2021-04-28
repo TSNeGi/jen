ec6c8feb2161c3d1b22702b3949d8fa39d9d6a3c
Tarun <tarunegy@gmail.com>
2021-04-28 19:23:31 +0530

    For Jenkins

---
+++ src/main/java/io/spring/deployment/controller/SpringController.java
@@ -0,0 +1,15 @@
+package io.spring.deployment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;  
+@RestController 
+
+public class SpringController {
+	
+	@RequestMapping("/")  
+	public String welcome()   
+	{  
+	String print="Successfully build pipeline in Jenkins. \n Its AWS with jenkins."; 
+	return print;
+	}  
+}
